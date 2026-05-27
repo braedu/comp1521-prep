04bf4138f5c5c4c3038dc1037a389b0b439a9155
braedu <sendeul@gmail.com>
2026-05-27 04:26:16 +0000

    struct lesson

--- structs2.c
+++ structs2.c
@@ -1,10 +1,30 @@
 #include <stdio.h>
-#include <strings.h>
 
-
+struct Student {
+    char name[50];
+    int age;
+    float grade;
+};
 
 int main(void) {
+    struct Student students[3];
 
+    printf("Enter the details for student\n");
+
+    for (int j = 0; j < 3; j++) {
+        printf("Student %d's name\n", j + 1);
+        scanf("%s", students[j].name);
+        printf("Student %d's age\n", j + 1);
+        scanf("%d", &students[j].age);
+        printf("Student %d's grade\n", j + 1);
+        scanf("%f", &students[j].grade);
+    }
+
+    for (int i = 0; i < 3; i++) {
+        printf("Student %d's name is %s\n", i + 1, students[i].name);
+        printf("Student %d's age is %d\n", i + 1, students[i].age);
+        printf("Student %d's grade is %.2f\n", i + 1, students[i].grade);
+    }
 
     return 0;
 }
---
+++ structs3.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+struct Student {
+    char name[50];
+    int age;
+    float grade;
+};
+
+printStudent(struct Student s);
+struct Student topStudent(struct Student students[], int size);
+
+int main(void) {
+    struct Student students[3];
+
+    for (int i = 0; i < 3; i++) {
+        printf("Please enter student %d's details\n", i + 1);
+    
+        printf("Student's name: ");
+        scanf("%s", students[i].name);
+        printf("Student's age: ");
+        scanf("%d", &students[i].age);
+        printf("Student's grade: ");
+        scanf("%f", &students[i].grade);
+    }
+
+    struct Student temp = topStudent(students, 3);
+    printStudent(temp);    
+
+    return 0;
+}
+
+void printStudent(struct Student s) {
+
+    printf("The student's name is %s\n", s.name);
+    printf("The student's age is %d\n", s.age);
+    printf("The student's grade is %f\n", s.grade);
+
+}
+
+struct Student topStudent(struct Student students[], int size) {
+    int temp = 0;
+
+    for (int i = 0; i < size - 1; i++ ) {
+            if (students[i].grade > students[i + 1].grade) {
+                temp = i;
+            } else {
+                temp = i + 1;
+            }
+    }
+    return students[temp];
+}
